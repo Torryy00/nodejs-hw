6d66d210e4b0b4635fc36383d7a6f4c8f8bc25f8
Mariia Prehaniuk <Votakmasha@gmail.com>
2026-06-28 19:20:25 +0200

    commit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,92 @@
 import createHttpError from 'http-errors';
 
+import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
+
 import { createSession, setSessionCookies } from '../services/auth.js';
+
+import bcrypt from 'bcryptjs';
+
+export const registerUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return next(createHttpError(409, 'Email in use'));
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      email,
+      password: hashedPassword,
+    });
+
+    const session = await createSession(newUser._id);
+
+    setSessionCookies(res, session);
+
+    return res.status(201).json({
+      user: {
+        email: newUser.email,
+        id: newUser._id,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return next(createHttpError(401, 'Email or password is wrong'));
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return next(createHttpError(401, 'Email or password is wrong'));
+    }
+
+    const session = await createSession(user._id);
+
+    setSessionCookies(res, session);
+
+    return res.status(200).json({
+      user: {
+        email: user.email,
+        id: user._id,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const logoutUser = async (req, res, next) => {
+  try {
+    const { sessionId } = req.cookies;
+
+    if (sessionId) {
+      await Session.deleteOne({ _id: sessionId });
+    }
+
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+    res.clearCookie('sessionId');
+
+    return res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const refreshUserSession = async (req, res, next) => {
   try {
@@ -27,13 +112,10 @@ export const refreshUserSession = async (req, res, next) => {
       res.clearCookie('refreshToken');
       res.clearCookie('sessionId');
 
-      return res.status(401).json({
-        message: 'Session token expired',
-      });
+      return next(createHttpError(401, 'Session token expired'));
     }
 
     const userId = session.userId;
-
 
     await Session.deleteOne({ _id: sessionId });
 
