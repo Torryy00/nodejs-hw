5395a5b3524069d9a2abb3300a28fb53062ee313
Mariia Prehaniuk <Votakmasha@gmail.com>
2026-06-28 19:36:47 +0200

    commit1

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 
-import authenticate from '../middlewares/authenticate.js';
+import authenticate from '../middleware/authenticate.js';
 
 import {
   getAllNotes,
